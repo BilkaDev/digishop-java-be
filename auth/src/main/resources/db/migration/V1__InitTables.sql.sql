bbdce7021c582111cac7094de18fb976647ea1f6
Marcin Bilka <bilkadev@gmail.com>
2024-06-01 15:56:30 +0200

    feat(auth): Added unique to login and email

--- auth/src/main/resources/db/migration/V1__InitTables.sql.sql
+++ auth/src/main/resources/db/migration/V1__InitTables.sql.sql
@@ -2,10 +2,10 @@ CREATE TABLE "users"
 (
     id        serial primary key,
     uuid      varchar not null,
-    login     varchar not null,
-    email     varchar not null,
+    login     varchar not null unique,
+    email     varchar not null unique,
     password  varchar not null,
     role      varchar not null,
     isLock    boolean DEFAULT true,
-    isEnabled boolean DEFAULT false,
-)
+    isEnabled boolean DEFAULT false
+);
