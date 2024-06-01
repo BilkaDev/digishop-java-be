f8c5fa2f00a12e559b596d91ade6b7a47ad4005b
Marcin Bilka <bilkadev@gmail.com>
2024-06-01 15:55:15 +0200

    feat(auth): Added new code response

--- auth/src/main/java/pl/networkmanager/bilka/auth/entity/Code.java
+++ auth/src/main/java/pl/networkmanager/bilka/auth/entity/Code.java
@@ -7,7 +7,14 @@ import lombok.Getter;
  */
 @Getter
 public enum Code {
-    SUCCESS("Operation end success");
+    SUCCESS("Operation end success"),
+    PERMIT("Access granted"),
+    A1("The specified user with the given name does not exist or hast not activated the account"),
+    A2("The specified data is invalid"),
+    A3("The indicated token is empty or not valid"),
+    A4("The user with the specified name does not exist"),
+    A5("The user with the specified email already exists"),
+    A6("The user does not exist");
     private final String label;
 
     Code(final String label) {
