48b5b88b14708ae4cdd1b4717ad8e700b4a86a79
Marcin Bilka <bilkadev@gmail.com>
2024-06-22 04:16:04 +0200

    feat(product): added product mediator

--- product/src/integration/java/pl/networkmanager/bilka/BaseIntegrationTest.java
+++ product/src/integration/java/pl/networkmanager/bilka/BaseIntegrationTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -11,6 +12,7 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.networkmanager.bilka.product.ProductApplication;
+import pl.networkmanager.bilka.product.infrastructure.configuration.ApiGatewayEndpointConfiguration;
 
 
 @SpringBootTest(classes = {ProductApplication.class, IntegrationConfiguration.class})
@@ -26,6 +28,8 @@ public class BaseIntegrationTest {
     @Container
     public static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16");
 
+    @MockBean
+    private ApiGatewayEndpointConfiguration apiGatewayEndpointConfiguration;
 
     @DynamicPropertySource
     public static void setDatasourceProperties(DynamicPropertyRegistry registry) {
