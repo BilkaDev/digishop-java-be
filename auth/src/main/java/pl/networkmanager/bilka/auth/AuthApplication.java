930653b23cd93f435d9e6eb1c18413ddf1144134
Marcin Bilka <bilkadev@gmail.com>
2024-06-22 05:11:18 +0200

    feat(basket): added error handler and register ep to dep

--- auth/src/main/java/pl/networkmanager/bilka/auth/AuthApplication.java
+++ auth/src/main/java/pl/networkmanager/bilka/auth/AuthApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 @ComponentScan(basePackages = {"pl.networkmanager.bilka.auth", "pl.networkmanager.bilka.errorhandler"})
 public class AuthApplication {
 
--- basket/src/main/java/pl/networkmanager/bilka/basket/BasketApplication.java
+++ basket/src/main/java/pl/networkmanager/bilka/basket/BasketApplication.java
@@ -2,8 +2,14 @@ package pl.networkmanager.bilka.basket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {"pl.networkmanager.bilka.basket", "pl.networkmanager.bilka.errorhandler"})
 public class BasketApplication {
 
     public static void main(String[] args) {
